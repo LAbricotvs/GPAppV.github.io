2fcb7515f921756962cc0991a864b92881b26c38
LAbricotvs <106334930+LAbricotvs@users.noreply.github.com>
2024-10-28 11:09:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -223,7 +223,7 @@ if (description.includes("dernières dates de service entretien")) {
      // Ajouter un texte juste avant les cases à cocher
     const serviceTestsLabel = document.createElement('p');
     serviceTestsLabel.textContent = '!! Prendre en photo le carnet de service !!';
-    serviceTestsLabel.style.cssText = 'color: red; font-weight: bold;';
+    serviceTestsLabel.classList.add('red-text');
     controlItem.appendChild(serviceTestsLabel);
     
     // Ajouter les cases à cocher
